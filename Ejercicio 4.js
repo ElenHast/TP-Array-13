8e724f6eb345e245ae73b27e6594f3e0b2efbf15
MRM <55654081+ElenHast@users.noreply.github.com>
2020-09-22 20:01:19 -0300

    Create Ejercicio 4.js

---
+++ Ejercicio 4.js
@@ -0,0 +1,7 @@
+/* 4 Debe modificarse el desarrollo del punto anterior para permitir:
+● Cargar un array con x múltiplos del número n, siendo que x y n son
+valores que ingresa el usuario por teclado. Se deben imprimir los
+valores calculados.
+● Repetir el proceso hasta que el usuario ingrese 0 para la cantidad de
+múltiplos a generar, es decir, cuando x = 0.
+ */
