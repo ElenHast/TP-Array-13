d5f3aeb120baad19e0e12c5e01cd26737431b940
MRM <55654081+ElenHast@users.noreply.github.com>
2020-09-22 20:00:24 -0300

    Create Ejercicio 2.js

---
+++ Ejercicio 2.js
@@ -0,0 +1,9 @@
+/* 2 Escribir una función que permita cargar un vector de 10 elementos y los
+imprima en orden inverso al de la carga.
+ */
+function imprimirAlrevez(array) {
+  let darvuelta = array.reverse(); 
+  return console.log(darvuelta);
+}
+
+imprimirAlrevez([1,2,3,4,5,6,7,8,9,0]);
