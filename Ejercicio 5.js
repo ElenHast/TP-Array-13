156fc56a470a3d0df29c74d9c5ba4b7017e322ba
MRM <55654081+ElenHast@users.noreply.github.com>
2020-09-22 20:01:43 -0300

    Create Ejercicio 5.js

---
+++ Ejercicio 5.js
@@ -0,0 +1,4 @@
+/* 5 Realizar una función que permita cargar un array de 20 posiciones con
+números enteros. Se debe retornar aquellas posiciones en las que el valor
+contenido sea múltiplo de 3.
+ */
