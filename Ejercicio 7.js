414e13b275ddb8cd1522aa6447a7fa589a12ec0b
MRM <55654081+ElenHast@users.noreply.github.com>
2020-09-22 20:02:22 -0300

    Create Ejercicio 7.js

---
+++ Ejercicio 7.js
@@ -0,0 +1,3 @@
+ /* 7 Se ingresan datos que indican la cantidad de personas que se registran en un
+hotel cada día de enero. Se necesita contar con una estadística que informe
+el promedio de personas ingresadas por día, en todo el mes. */
