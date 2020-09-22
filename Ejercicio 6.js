1b324d64a9f23d90781cbb4bd94dc3212a59bcbe
MRM <55654081+ElenHast@users.noreply.github.com>
2020-09-22 20:02:02 -0300

    Create Ejercicio 6.js

---
+++ Ejercicio 6.js
@@ -0,0 +1,9 @@
+ /* 6 Realizar un script que incluya dos funciones.
+● Función "ingresarDatos": permite cargar en un array las notas de los
+15 exámenes finales de los alumnos que cursaron la cátedra
+“Introducción a la Informática” durante el 1er cuatrimestre de 2020.
+● Función “procesarNotas": recibe el array y deberá procesar las notas
+para informar: el promedio, la nota más baja, la nota más alta y las
+cantidades de aprobados y desaprobados (considerar que se aprueba
+con 6).
+ */
