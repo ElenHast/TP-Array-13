fa5893f1b36246dacff71f7fa300b378bb5081ff
MRM <55654081+ElenHast@users.noreply.github.com>
2020-09-23 17:30:27 -0300

    Update Ejercicio 5.js

--- Ejercicio 5.js
+++ Ejercicio 5.js
@@ -2,3 +2,37 @@
 números enteros. Se debe retornar aquellas posiciones en las que el valor
 contenido sea múltiplo de 3.
  */
+
+//TODAVIA NO FUNCIONA 
+ function multiplo3(items) {
+   items = []
+   let x;
+   let limite = 0;
+   let multiplos3 = [];
+   let i = 1;
+   let multiplo;
+   //Se insertan los valores en items para su comparacion
+   while (x != 0) {
+     x = parseInt(prompt("Inserte valores - Para parar inserte 0 "));
+     items.push(x);
+   }
+   //Se consiguen los multiplos
+   while (limite < 21) {
+      multiplos3.push(multiplo)
+      i++;
+      multiplo = 3 * i;
+      limite++;
+    }
+    //Comienza la comparacion
+    limite = 0;
+    let index = 0;
+    let coincidencias = [];
+    while (limite < 21) {
+      if (items[index] == multiplos3) {
+        coincidencias.push(items[index])
+      }
+      limite++;
+    } return console.log("Coincidencias "+ coincidencias);
+ }
+
+ multiplo3();
