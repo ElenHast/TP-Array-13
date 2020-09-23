65bbfed2240f9c32df716a6bbea72f1a99ba5c75
MRM <55654081+ElenHast@users.noreply.github.com>
2020-09-23 16:34:04 -0300

    Update Ejercicio 3.js

--- Ejercicio 3.js
+++ Ejercicio 3.js
@@ -3,10 +3,12 @@ cargado con los primeros 20 múltiplos del número n.
  */
  function multiplosEnArray(numeroN) {
    let items = [];
+   let i = 1;
    let limite = 0;
    let multiplo = numeroN
    while (limite < 21) {
      items.push(multiplo)
+     i++;
      multiplo = numeroN * i;
      limite++;
    } return console.log("Los multiplos de "+numeroN+" son ",items);
