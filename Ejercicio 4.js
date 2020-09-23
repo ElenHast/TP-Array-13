b477f4491e0774174b912a427fedc644a29999f2
MRM <55654081+ElenHast@users.noreply.github.com>
2020-09-23 16:37:18 -0300

    Update Ejercicio 4.js

--- Ejercicio 4.js
+++ Ejercicio 4.js
@@ -5,3 +5,17 @@ valores calculados.
 ● Repetir el proceso hasta que el usuario ingrese 0 para la cantidad de
 múltiplos a generar, es decir, cuando x = 0.
  */
+function multiplosEnArray(numeroN,limiteMultiplos) {
+  let items = [];
+  let i = 1;
+  let limite = 0;
+  let multiplo = numeroN
+  while (limite < limiteMultiplos) {
+    items.push(multiplo)
+    i++;
+    multiplo = numeroN * i;
+    limite++;
+  } return console.log("Numero de multiples pedidos = "+limiteMultiplos+ " Los multiplos de "+numeroN+" son ",items);
+}
+
+multiplosEnArray(2,0);
